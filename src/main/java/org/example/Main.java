9df8626289c13bb60fdb13d3bebd50f0d0e595e6
Ömer Kenar <omer.ken@hotmail.com>
2024-09-17 00:35:42 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,6 +20,7 @@ public class Main {
         Wall wall = new Wall(5,4);
 
         System.out.println("area= " + wall.getArea());
+        System.out.println("width= " + wall.getWidth());
 
         wall.setHeight(-1.5);
 
